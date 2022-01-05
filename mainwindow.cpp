c8cf9ebc6232ab79a00bce17dbfe190b08c32816
Crawcik <crawcik@gmail.com>
2022-01-05 12:41:07 +0100

    "Add references to project" function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,9 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui_list = findChild<QListWidget*>("list", Qt::FindChildrenRecursively);
+    apply_button = findChild<QPushButton*>("apply", Qt::FindChildrenRecursively);
     ui_list->setEnabled(false);
+    apply_button->setEnabled(false);
 
     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
     connect(manager, &QNetworkAccessManager::finished, this, &MainWindow::GetRequest);
@@ -26,49 +28,110 @@ void MainWindow::GetRequest(QNetworkReply *reply)
     reply->deleteLater();
     QJsonDocument doc = QJsonDocument::fromJson(replyText.toUtf8());
     QJsonArray arr = doc.array();
-    items = new QList<Item>();
+    items = new QList<Item*>();
+    cachedItems = new QList<Item*>();
     for (auto val : arr)
     {
         QJsonObject obj = val.toObject();
-        Item item = {
-            .name = obj["name"].toString(),
-            .path = obj["projectFile"].toString(),
-            .ui = new QListWidgetItem(ui_list)
-        };
-        item.ui->setText(item.name);
-        item.ui->setToolTip(obj["description"].toString());
-        item.ui->setCheckState(Qt::Unchecked);
+        Item *item = new Item();
+        item->name = obj["name"].toString(),
+        item->path = obj["projectFile"].toString(),
+        item->moduleName = obj["moduleName"].toString(),
+        item->moduleEditorName = obj["moduleEditorName"].toString(),
+        item->ui = new QListWidgetItem(ui_list);
+        item->ui->setText(item->name);
+        item->ui->setToolTip(obj["description"].toString());
+        item->ui->setCheckState(Qt::Unchecked);
         items->append(item);
     }
 }
 
 void MainWindow::on_select_clicked()
 {
+    apply_button->setEnabled(false);
     filename = QFileDialog::getOpenFileName(this,
         tr("Open Flax project"), nullptr, tr("Flaxproj (*.flaxproj)"));
     QFile file(filename);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-            return;
+    {
+        QMessageBox::warning(this, "Error", "Cannot read this file!");
+        return;
+    }
     setWindowTitle(file.fileName());
     ui_list->setEnabled(true);
     QString content = file.readAll();
+    file.close();
     QJsonDocument doc = QJsonDocument::fromJson(content.toUtf8());
     QJsonArray arr = doc["References"].toArray();
     for (auto val : arr)
     {
         QString path = val.toObject()["Name"].toString();
-        if(path.contains("$(ProjectPath)/Plugins/"))
+        for(int i = 0; i < items->count(); i++)
         {
-            path = path.remove(0, sizeof("$(ProjectPath)/Plugins/"));
-        }
-        for (int i = 0; i < items->count(); i++)
-        {
-            Item item = items->at(i);
-            if(path.contains(item.path))
+            Item* item = items->at(i);
+            if(path.contains(item->path))
             {
-                item.ui->setCheckState(Qt::Checked);
+                item->ui->setCheckState(Qt::Checked);
+                cachedItems->append(item);
             }
 
         }
     }
+    apply_button->setEnabled(true);
 }
+
+void MainWindow::on_apply_clicked()
+{
+    QFile file(filename);
+    QDir directory = QFileInfo(filename).absoluteDir();
+    if (!file.open(QIODevice::ReadWrite | QIODevice::Text))
+    {
+        QMessageBox::warning(this, "Error", "Cannot read/write in flaxproj file!");
+        return;
+    }
+    if(!directory.exists("Plugins"))
+    {
+        directory.mkdir("Plugins");
+    }
+    ui_list->setEnabled(true);
+    QString content = file.readAll();
+    QJsonDocument doc = QJsonDocument::fromJson(content.toUtf8());
+    QJsonObject root = doc.object();
+    QJsonArray arr = root["References"].toArray();
+    for(int i = 0; i < items->count(); i++)
+    {
+        Item* item = items->at(i);
+        bool checked = item->ui->checkState() == Qt::Checked;
+        // XNOR gate. cached contains means its already installed
+        if(cachedItems->contains(item) == checked)
+            continue;
+        if(checked)
+        {
+            QJsonObject obj;
+            obj.insert("Name", QJsonValue("$(ProjectPath)/Plugins/" + item->path));
+            cachedItems->append(item);
+            arr.append(obj);
+        }
+        else
+        {
+            for(int j = 0; j < arr.count(); j++)
+            {
+                QJsonObject arrObj = arr[j].toObject();
+                if(arrObj["Name"].toString().contains(item->path))
+                {
+                    arr.removeAt(j);
+                    cachedItems->removeOne(item);
+                    break;
+                }
+            }
+        }
+    }
+    root["References"] = arr;
+    doc.setObject(root);
+    file.resize(0);
+    file.seek(0);
+    file.write(doc.toJson(QJsonDocument::JsonFormat::Indented));
+    file.close();
+    apply_button->setEnabled(true);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -3,12 +3,14 @@
 
 #include <QMainWindow>
 #include <QListWidget>
+#include <QPushButton>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonArray>
 #include <QFileDialog>
+#include <QMessageBox>
 
 #define JSON_URL "https://raw.githubusercontent.com/Crawcik/FlaxPluginManager/master/plugin_list.json"
 
@@ -23,6 +25,8 @@ class MainWindow : public QMainWindow
     typedef struct _Item {
         QString name;
         QString path;
+        QString moduleName;
+        QString moduleEditorName;
         QListWidgetItem *ui;
     } Item;
 
@@ -33,13 +37,14 @@ public:
 private slots:
     void GetRequest(QNetworkReply *reply);
     void on_select_clicked();
+    void on_apply_clicked();
 
 private:
     Ui::MainWindow *ui;
     QListWidget *ui_list;
+    QPushButton *apply_button;
     QString filename;
-    QList<Item> *items;
-
-
+    QList<Item*> *items;
+    QList<Item*> *cachedItems;
 };
 #endif // MAINWINDOW_H
